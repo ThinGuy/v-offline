c0d8dc725656f353e69d126a0ef85aef23130fd4
craig bender <19372989+ThinGuy@users.noreply.github.com>
2021-01-19 06:05:18 -0800

    Fetch meta-release{-lts} in postmirror.sh

--- apt-mirror/postmirror.sh
+++ apt-mirror/postmirror.sh
@@ -4,9 +4,9 @@
 printf "\nRunning Post mirror script...\n"
 
 #Run clean script
-[[ -x ${AIRSTACK_DIR}/var/clean.sh ]] && printf "\n\e[2G - Running apt-mirror clean script...\n"
-[[ -x ${AIRSTACK_DIR}/var/clean.sh ]] && ${AIRSTACK_DIR}/var/clean.sh
-[[ -x ${AIRSTACK_DIR}/var/clean.sh ]] && { printf "\napt-mirror mirror script has completed\n" 2>&1; }
+[[ -x /srv/mirror/var/clean.sh ]] && printf "\n\e[2G - Running apt-mirror clean script...\n"
+[[ -x /srv/mirror/var/clean.sh ]] && /srv/mirror/var/clean.sh
+[[ -x /srv/mirror/var/clean.sh ]] && { printf "\napt-mirror mirror script has completed\n" 2>&1; }
 
 wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/meta-release https://changelogs.ubuntu.com/meta-release
 wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/meta-release-lts https://changelogs.ubuntu.com/meta-release-lts
