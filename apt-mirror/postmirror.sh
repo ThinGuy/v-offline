4a25ce350b70c44e61192f370b07740dab8ab924
craig bender <19372989+ThinGuy@users.noreply.github.com>
2021-01-19 06:20:21 -0800

    Fetch latest do-release-upgrade in postmirror.sh

--- apt-mirror/postmirror.sh
+++ apt-mirror/postmirror.sh
@@ -11,5 +11,19 @@ printf "\nRunning Post mirror script...\n"
 wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/meta-release https://changelogs.ubuntu.com/meta-release
 wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/meta-release-lts https://changelogs.ubuntu.com/meta-release-lts
 
+mkdir -p /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/
+mkdir -p /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/xenial-updates/main/dist-upgrader-all/current/
+mkdir -p /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/dist-upgrader-all/current/
+mkdir -p /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/focal-updates/main/dist-upgrader-all/current/
+
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/trusty.tar.gz http://us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/trusty.tar.gz
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/trusty.tar.gz.gpg http://us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/trusty.tar.gz.gpg
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/xenial-updates/main/dist-upgrader-all/current/xenial.tar.gz http://us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/xenial.tar.gz
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/xenial-updates/main/dist-upgrader-all/current/xenial.tar.gz.gpg http://us.archive.ubuntu.com/ubuntu/dists/trusty-updates/main/dist-upgrader-all/current/xenial.tar.gz.gpg
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/dist-upgrader-all/current/bionic.tar.gz http://us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/dist-upgrader-all/current/bionic.tar.gz
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/dist-upgrader-all/current/bionic.tar.gz.gpg http://us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/dist-upgrader-all/current/bionic.tar.gz.gpg
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/focal-updates/main/dist-upgrader-all/current/focal.tar.gz http://us.archive.ubuntu.com/ubuntu/dists/focal-updates/main/dist-upgrader-all/current/focal.tar.gz
+sudo wget -qO /srv/mirror/archives/us.archive.ubuntu.com/ubuntu/dists/focal-updates/main/dist-upgrader-all/current/focal.tar.gz.gpg http://us.archive.ubuntu.com/ubuntu/dists/focal-updates/main/dist-upgrader-all/current/focal.tar.gz.gpg
+
 
 printf "\nPost-mirror script has completed\n" 2>&1
