3f26dc3731f4244a955c3a112c36f0e05cf0efe7
craig bender <19372989+ThinGuy@users.noreply.github.com>
2021-01-19 05:37:18 -0800

    Add files via upload

--- apt-mirror/postmirror.sh
+++ apt-mirror/postmirror.sh
@@ -4,9 +4,9 @@
 printf "\nRunning Post mirror script...\n"
 
 #Run clean script
-[[ -x /srv/mirror/var/clean.sh ]] && printf "\n\e[2G - Running apt-mirror clean script...\n"
-[[ -x /srv/mirror/var/clean.sh ]] && /srv/mirror/var/clean.sh
-[[ -x /srv/mirror/var/clean.sh ]] && { printf "\napt-mirror mirror script has completed\n" 2>&1; }
+[[ -x ${AIRSTACK_DIR}/var/clean.sh ]] && printf "\n\e[2G - Running apt-mirror clean script...\n"
+[[ -x ${AIRSTACK_DIR}/var/clean.sh ]] && ${AIRSTACK_DIR}/var/clean.sh
+[[ -x ${AIRSTACK_DIR}/var/clean.sh ]] && { printf "\napt-mirror mirror script has completed\n" 2>&1; }
 
 
 printf "\nPost-mirror script has completed\n" 2>&1
