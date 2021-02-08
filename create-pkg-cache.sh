e060787005feb5b100a3f1be5736bbd3f8de5ce9
craig bender <19372989+ThinGuy@users.noreply.github.com>
2021-02-08 08:09:24 -0800

    Update create-pkg-cache.sh
    fix rsync command

--- create-pkg-cache.sh
+++ create-pkg-cache.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export VERSA_GIT="/home/ubuntu/v-offline"
 export VERSA_PKG_LISTS=${VERSA_GIT}/package-listings
 export VERSA_POOL_LISTS=${VERSA_GIT}/pool-file-listings
@@ -8,7 +7,7 @@ export VERSA_ARCHIVES="${VERSA_DIR}/archives"
 declare -ag VERSA_DISTS=(trusty xenial bionic)
 export VERSA_PKG_CACHE="/home/ubuntu/versa-local/package-cache"
 mkdir -p ${VERSA_PKG_CACHE}
-
+#eval cat ${VERSA_POOL_LISTS}/pool.files.${DIST}.txt|xargs -rn1 -P0 bash -c 'echo ${0%%*/};echo rsync -r --info=progress2 --info=name0 '${VERSA_ARCHIVES}'/us.archive.ubuntu.com/ubuntu/${0} '${VERSA_PKG_CACHE}'/${0}'
 for DIST in ${VERSA_DISTS[@]};do
-	eval cat ${VERSA_POOL_LISTS}/pool.files.${DIST}.txt|xargs -rn1 -P0 bash -c 'rsync -r --info=progress2 --info=name0 '${VERSA_ARCHIVES}'/us.archive.ubuntu.com/ubuntu/${0} '${VERSA_PKG_CACHE}'/${0}'
+	eval cat ${VERSA_POOL_LISTS}/pool.files.${DIST}.txt|xargs -rn1 -P0 bash -c 'mkdir -p '${VERSA_PKG_CACHE}'/${0%/*};rsync -r --info=progress2 --info=name0 '${VERSA_ARCHIVES}'/us.archive.ubuntu.com/ubuntu/${0} '${VERSA_PKG_CACHE}'/${0}'
 done
